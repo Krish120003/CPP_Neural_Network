f665cfa545f532e9f87ab29ac813f29f6bb27662
Krish120003 <krish120003@gmail.com>
2023-10-28 21:24:32 -0400

    feat: add backward functionality for CEL

--- src/engine/CrossEntropyLoss.cpp
+++ src/engine/CrossEntropyLoss.cpp
@@ -6,6 +6,12 @@ public:
     CrossEntropyLoss();
     ~CrossEntropyLoss();
     double forward(std::vector<double> inputs, int target);
+    void backward(double grad);
+
+    // data
+    std::vector<double> last_input;
+    int last_target;
+    std::vector<double> grad;
 };
 
 CrossEntropyLoss::CrossEntropyLoss()
@@ -19,6 +25,16 @@ CrossEntropyLoss::~CrossEntropyLoss()
 double CrossEntropyLoss::forward(std::vector<double> inputs, int target)
 {
     // we only need to calculate the loss for the target class
+    this->last_input = inputs;
+    this->last_target = target;
+
     double loss = -log(inputs.at(target));
     return loss;
+}
+
+void CrossEntropyLoss::backward(double grad)
+{
+    // we only need to calculate the gradient for the target class
+    this->grad = std::vector<double>(this->last_input.size());
+    this->grad[this->last_target] = -1.0 / this->last_input[this->last_target];
 }
